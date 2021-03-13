4ade2dea9fa75ea852bd2276fec6347b916ce5e2
noah <noah071610@naver.com>
2021-03-13 20:45:21 +0900

    theme delete and combine  portfolio with blog

--- src/components/App.js
+++ src/components/App.js
@@ -1,60 +1,68 @@
-import { useSelector } from "react-redux";
-import React, { Suspense, useEffect, useState } from "react";
+import React, { Suspense } from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 import PortfolioMainPage from "./views/PortfolioMainPage/PortfolioMainPage";
-import { ThemeProvider } from "styled-components";
+import { createGlobalStyle } from "styled-components";
 import PortfolioPostPage from "./views/PortfolioMainPage/PortfolioSection/PortfolioPostPage";
 import AOS from "aos";
 import "aos/dist/aos.css";
-import { darkTheme, lightTheme, GlobalStyles } from "./themes";
+import "antd/dist/antd.css";
 import BlogMainPage from "./views/BlogMainPage/BlogMainPage";
 import BlogTechPage from "./views/BlogMainPage/BlogTechPage/BlogTechPage";
 import BlogPostPage from "./views/BlogMainPage/BlogPostPage/BlogPostPage";
 import BlogClassPage from "./views/BlogMainPage/BlogClassPage/BlogClassPage";
+import PageWrapper from "./views/BlogMainPage/_common/PageWrapper";
+
+const GlobalStyles = createGlobalStyle`
+
+.ant-row {
+  margin-right: 0 !important;
+  margin-left: 0 !important;
+}
+
+.ant-table-pagination{
+  display:none !important;
+}
+
+p{
+  margin:0.5rem auto;
+}
+h1,
+h2,
+h3 {
+  margin-top: 0.5rem;
+  margin-bottom: 0.5rem;
+  font-weight: 600;
+}
+h2{
+  font-size:1.1rem;
+}
+
+a {
+  color: black;
+  text-decoration: none;
+}
+
+`;
 
 const App = () => {
-  const [mobileSize, setmobileSize] = useState(null);
-  const { theme } = useSelector((state) => state);
-
-  useEffect(() => {
-    function mediaQuery() {
-      if (window.innerWidth >= 992) {
-        setmobileSize(false);
-      } else if (window.innerWidth < 992) {
-        setmobileSize(true);
-      }
-    }
-    window.addEventListener("resize", mediaQuery);
-    window.addEventListener("DOMContentLoaded", mediaQuery);
-    return () => {
-      window.removeEventListener("resize", mediaQuery);
-      window.removeEventListener("DOMContentLoaded", mediaQuery);
-    };
-  }, []);
   AOS.init({ offset: 120 });
   return (
-    <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
-      <GlobalStyles />
-      <Suspense fallback={<div>Loading...</div>}>
-        <BrowserRouter>
-          <Route exact path="/" component={BlogMainPage} />
-          <Route exact path="/tech" component={BlogTechPage} />
-          <Route exact path="/daily" component={BlogTechPage} />
-          <Route exact path="/class" component={BlogClassPage} />
-          <Route exact path="/post/:id" component={BlogPostPage} />
-          <Route
-            exact
-            path="/portfolio"
-            render={() => <PortfolioMainPage mobileSize={mobileSize} />}
-          />
-          <Route
-            exact
-            path="/portfolio/:id"
-            render={() => <PortfolioPostPage mobileSize={mobileSize} />}
-          />
-        </BrowserRouter>
-      </Suspense>
-    </ThemeProvider>
+    <>
+      <BrowserRouter>
+        <GlobalStyles />
+        <Suspense fallback={<div>Loading...</div>}>
+          <PageWrapper>
+            <Route exact path="/" component={BlogMainPage} />
+            <Route exact path="/tech" component={BlogTechPage} />
+            <Route exact path="/daily" component={BlogTechPage} />
+            <Route exact path="/class" component={BlogClassPage} />
+            <Route exact path="/post/:id" component={BlogPostPage} />
+            <Route exact path="/portfolio" component={PortfolioMainPage} />
+            <Route exact path="/portfolio/:id" component={PortfolioPostPage} />
+          </PageWrapper>
+        </Suspense>
+      </BrowserRouter>
+    </>
   );
 };
 
